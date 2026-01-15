c00edb3c3246ce9c7385e480cca83f5e51a964ab
freemandaily <onahchinemelem@gmail.com>
2026-01-15 08:25:47 +0100

    docker compose added

--- Docker-workshop/pipeline/ingest_data.py
+++ Docker-workshop/pipeline/ingest_data.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import pandas as pd
+import click
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
@@ -31,21 +32,22 @@ parse_dates = [
 ]
 
 
-def run():
-    year = 2021
-    month = 1
-
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-    target_table ='yellow_taxi_data'
-    chunksize = 100000
+@click.command()
+@click.option('--year', default=2021, type=int, help='Year for the taxi data.')
+@click.option('--month', default=1, type=int, help='Month for the taxi data.')
+@click.option('--pg_user', default='root', help='PostgreSQL user.')
+@click.option('--pg_pass', default='root', help='PostgreSQL password.')
+@click.option('--pg_host', default='localhost', help='PostgreSQL host.')
+@click.option('--pg_port', default=5432, type=int, help='PostgreSQL port.')
+@click.option('--pg_db', default='ny_taxi', help='PostgreSQL database name.')
+@click.option('--target_table', default='yellow_taxi_data', help='Name of the target table in PostgreSQL.')
+@click.option('--chunksize', default=100000, type=int, help='Size of chunks to read from CSV.')
+def run(year, month, pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, chunksize):
+    print('started')
 
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+    print('engine created')
 
-        
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download'
     url = f'{prefix}/yellow/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
